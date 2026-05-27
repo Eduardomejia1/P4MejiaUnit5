a17591eb27ce586af5cb6e32e15e00ee7971b3e2
Eduardomejia1 <730845@student.dusd.net>
2026-05-27 00:43:52 -0700

    Added a Click And Swipe Script
    Added a click and swipe script to my project.

--- Unit5MejiaP4/Assets/Scripts/GameManager.cs
+++ Unit5MejiaP4/Assets/Scripts/GameManager.cs
@@ -11,23 +11,29 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
     public Button restartButton;
+    public GameObject titleScreen;
     public bool isGameActive;
-    private int score; 
+    private int score;
     private float spawnRate = 1.0f;
+    public TextMeshProUGUI livesText;
+    private int lives;
+    public GameObject pauseScreen;
+    private bool paused;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        isGameActive = true;
-        score = 0;
 
-        StartCoroutine(SpawnTarget());
-        UpdateScore(0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Check if the user has pressed the P key
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ChangePaused();
+        }
         
     }
 
@@ -47,6 +53,16 @@ public class GameManager : MonoBehaviour
         scoreText.text = "Score: " + score;
     }
 
+    public void UpdateLives(int livesToChange)
+    {
+        lives += livesToChange;
+        livesText.text = "Lives: " + lives;
+        if (lives <= 0)
+        {
+            GameOver();
+        }
+    }
+
     public void GameOver()
     {
         restartButton.gameObject.SetActive(true);
@@ -57,5 +73,34 @@ public class GameManager : MonoBehaviour
     public void RestartGame()
     { 
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        score = 0;
+        spawnRate /= difficulty;
+
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+        UpdateLives(3);
+
+        titleScreen.gameObject.SetActive(false);
+    }
+
+    void ChangePaused()
+    {
+        if (!paused)
+        {
+            paused = true;
+            pauseScreen.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            paused = false;
+            pauseScreen.SetActive(false);
+            Time.timeScale = 1;
+        }
     }
 }
--- Unit5MejiaP4/Assets/Scripts/Target.cs
+++ Unit5MejiaP4/Assets/Scripts/Target.cs
@@ -47,11 +47,21 @@ public class Target : MonoBehaviour
     {
         Destroy(gameObject);
 
-        if(!gameObject.CompareTag("Bad"))
+        if(!gameObject.CompareTag("Bad") && gameManager.isGameActive)
         {
-            gameManager.GameOver();
+            gameManager.UpdateLives(-1);
         }
        
+    }
+
+    public void DestroyTarget()
+    {
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
     }
 
     Vector3 RandomForce()
